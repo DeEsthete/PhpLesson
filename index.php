83bf50bf1be9e8dcb18f8684c480b222b966c920
JekaLion <djoni-01@mail.ru>
2019-09-10 21:43:15 +0600

    init

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php //язык сатаны
+
+// //$arr = [1, "leha", true, 123.321]; //create array
+// //or
+// $dictionary = [
+//   "name" => "leha",
+//   "age" => 21
+// ];
+// echo $dictionary["name"];
+// echo $dictionary[count($dictionary)-1];
+//=========================================================================
+// $a = [
+//   "Ivanov", "Petr", "Vladimirovich"
+// ];
+
+// $b = $a[0] . " " . $a[1][0] . "." . $a[2][0] . ".";
+// echo $b;
+// echo $a[0][0];
+
+
+// $str = "5724998786787";
+// if (strlen($str) > 10)
+// {
+//   echo ">10";
+// }
+// else
+// {
+//   echo "<10";
+// }
+//=========================================================================
+// $num = 2234010101;
+// $num = (string)$num;
+// $index = 3;
+// echo ($index < strlen($num) && $index >= 0 ? ($num[$index] % 2 == 0 ? "chet" : "nechet") : "not valid");
+// if ($index < strlen($num))
+// {
+//   if ($num[$index] % 2 == 0)
+//   {
+//     echo "chet";
+//   }
+//   else
+//   {
+//     echo "nechet";
+//   }
+// }
+//=========================================================================
+// $a = 3;
+// echo ($a == 5 ? "equal" : "not equal");
+
+$day = 4;
+switch ($a) {
+  case 6:
+    echo "vih";
+    break;
+  case 7:
+    echo "vih";
+    break;
+  default:
+  echo "budniy";
+    break;
+}
