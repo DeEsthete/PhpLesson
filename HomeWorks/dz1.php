22ae57fe55880b373a44e57a0954f2d3b11dfaa3
JeLion <djoni-01@mail.ru>
2019-09-12 17:26:47 +0600

    Add dz2 and sort files

---
+++ HomeWorks/dz1.php
@@ -0,0 +1,40 @@
+<?php
+//========================1========================
+
+//$a = 123;
+//if (is_int($a))
+//{
+//  $a = (string)$a;
+//  $answer = 0;
+//  for ($i=0; $i < strlen($a); $i++) { 
+//    $answer = $answer + $a[$i];
+//  }
+//  echo "1)Answer: " . $answer;
+//}
+
+//========================2========================
+
+//$requiredNum = 4;
+//$inNum = 474794;
+//$count = 0;
+//$requiredNum = (string)$requiredNum;
+//$inNum = (string)$inNum;
+//for ($i = 0; $i < strlen($inNum); $i++){
+//    if ($inNum[$i] == $requiredNum){
+//        $count++;
+//    }
+//}
+//echo "2)Answer: " . $count;
+
+//========================3========================
+
+// $a = [
+//   "Ivanov", "Petr", "Vladimirovich"
+// ];
+// $b = $a[0] . " " . $a[1][0] . "." . $a[2][0] . ".";
+// echo "3)Answer: " . $b;
+
+//========================4========================
+
+//$arr = ['Hello', 'world', '!'];
+//echo $arr[0] . " " . $arr[1] . " " . $arr[2];
